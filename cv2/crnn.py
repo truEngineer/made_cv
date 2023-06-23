d1105481b3cfa9cd3c45658b680efa54e8e446f8
truEngineer <62888379+truEngineer@users.noreply.github.com>
2023-06-23 18:45:17 +0300

    Add CRNN sequence predictor

--- cv2/crnn.py
+++ cv2/crnn.py
@@ -19,7 +19,7 @@ class FeatureExtractor(nn.Module):
         # state_dict = torch.load('weights/path/efficientnet_v2_s-dd5fe13b.pth')
         # efficientnet.load_state_dict(state_dict)
 
-        self.cnn = nn.Sequential(*list(efficientnet.children())[:-2])
+        self.cnn = nn.Sequential(*list(efficientnet.children())[:-2])  # resnet.children()
         self.pool = nn.AvgPool2d(kernel_size=(h // 32, 1))
         self.proj = nn.Conv2d(w // 32, output_len, kernel_size=1)  # 320 // 32
         # in_channels, out_channels, kernel_size, stride=1,
@@ -45,3 +45,45 @@ class FeatureExtractor(nn.Module):
         # Apply projection to increase width
         features = self.apply_projection(features)
         return features
+
+
+class SequencePredictor(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes,
+                 dropout=0.3, bidirectional=False):
+        super().__init__()
+        self.num_classes = num_classes
+        self.rnn = nn.GRU(
+            input_size=input_size, hidden_size=hidden_size,
+            num_layers=num_layers, dropout=dropout,
+            bidirectional=bidirectional
+        )
+        fc_in = hidden_size if not bidirectional else 2 * hidden_size
+        self.fc = nn.Linear(in_features=fc_in, out_features=num_classes)
+
+    def _init_hidden_(self, batch_size):
+        """Initialize new tensor of zeroes for RNN hidden state.
+        Accepts batch size.
+        Returns tensor of zeros shaped
+        (num_layers * num_directions, batch, hidden_size).
+        """
+        num_directions = 2 if self.rnn.bidirectional else 1
+        return torch.zeros(self.rnn.num_layers * num_directions,
+                           batch_size, self.rnn.hidden_size)
+
+    def _prepare_features_(self, x):
+        """Change dimensions of x to fit RNN expected input.
+        Accepts tensor x shaped (B x (C=1) x H x W).
+        Returns new tensor shaped (W x B x H).
+        """
+        x = x.squeeze(1)
+        x = x.permute(2, 0, 1)
+        return x
+
+    def forward(self, x):
+        x = self._prepare_features_(x)
+        batch_size = x.size(1)
+        h_0 = self._init_hidden_(batch_size)
+        h_0 = h_0.to(x.device)
+        x, h = self.rnn(x, h_0)
+        x = self.fc(x)
+        return x
