1d474312386ddcee2b5bbaaf1543566866fb40a6
truEngineer <62888379+truEngineer@users.noreply.github.com>
2023-06-26 22:41:44 +0300

    Add CRNN class

--- cv2/crnn.py
+++ cv2/crnn.py
@@ -87,3 +87,27 @@ class SequencePredictor(nn.Module):
         x, h = self.rnn(x, h_0)
         x = self.fc(x)
         return x
+
+
+class CRNN(nn.Module):
+    def __init__(self, alphabet,
+                 cnn_input_size=(64, 320), cnn_output_len=30,
+                 rnn_hidden_size=128, rnn_num_layers=2,
+                 rnn_dropout=0.3, rnn_bidirectional=False):
+        super().__init__()
+        self.alphabet = alphabet
+        self.features_extractor = FeatureExtractor(
+            input_size=cnn_input_size,
+            output_len=cnn_output_len
+        )
+        self.sequence_predictor = SequencePredictor(
+            input_size=self.features_extractor.num_output_features,
+            hidden_size=rnn_hidden_size, num_layers=rnn_num_layers,
+            num_classes=len(alphabet)+1,  # alphabet symbols + blank
+            dropout=rnn_dropout, bidirectional=rnn_bidirectional
+        )
+
+    def forward(self, x):
+        features = self.features_extractor(x)
+        sequence = self.sequence_predictor(features)
+        return sequence
