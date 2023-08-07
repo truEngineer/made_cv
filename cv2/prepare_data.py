565a3f76334cd7861ff6c550157da2f272b83a2c
truEngineer <62888379+truEngineer@users.noreply.github.com>
2023-08-07 12:45:34 +0300

    Add test data analysis

--- cv2/prepare_data.py
+++ cv2/prepare_data.py
@@ -5,7 +5,8 @@ import pandas as pd
 from data_analysis import (obtain_labeled_images_stats_inplace,
                            delete_small_images_samples_inplace,
                            delete_rare_symbols_samples_inplace,
-                           calc_images_hists)
+                           calc_images_hists, 
+                           get_test_df, show_64_test_images)
 
 IMG_SIZE_THRESH = 5
 SYM_FREQ_THRESH = 3
@@ -39,3 +40,12 @@ if __name__ == '__main__':
     labels_df.to_csv(data_path / f'extended_train_labels_st{IMG_SIZE_THRESH}_ft{SYM_FREQ_THRESH}.csv',
                      header=True, index=False)  # ft: freq threshold
     calc_images_hists(labels_df, columns, save_filename=f'images_hists_st{IMG_SIZE_THRESH}_ft{SYM_FREQ_THRESH}')
+
+    # 4. test data analysis
+    # print(sorted(test_data_path.glob('*'), key=lambda x: int(x.stem))[:100])
+    test_df = get_test_df(test_data_path)  # !!! time
+    test_df.to_csv(data_path / 'test_data.csv', header=True, index=False)
+    # test_df = pd.read_csv(data_path / f'test_data.csv')
+    show_64_test_images(test_data_path, test_df, first_idx=0, save=True)
+    columns = ['img_width', 'img_height', 'img_channels', 'img_ratio']
+    calc_images_hists(test_df, columns, save_filename='test_images_hists')
